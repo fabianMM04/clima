a9544cf7e9a07384073fdaf52b5bd7bb864c8d1a
fabianMM04 <fabianMM04@users.noreply.github.com>
2018-06-25 08:33:53 -0500

    Delete admin.py

--- clima/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Medicion, Coordenadas, Sensor
-
-# Register your models here.
-admin.site.register(Medicion)
-admin.site.register(Coordenadas)
-admin.site.register(Sensor)
